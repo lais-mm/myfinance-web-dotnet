5dd79113eef52a564d7287a26fb0664ef6baaac0
Laís Moreira <lais-mm@outlook.com>
2023-11-07 20:46:43 -0300

    configs and add entity framework core

---
+++ 2-) myfinance-web-dotnet-service/Interfaces/IPlanoContaService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myfinance_web_dotnet_service.Interfaces
+{
+    public interface IPlanoContaService
+    {
+        
+    }
+}
